83036b5660f573d84f88ca2514cd2fa100caf5b7
zhenghz@t-rech.com <291614517@qq.com>
2021-03-23 23:07:16 +0800

    修改包命名

---
+++ salary/start.py
@@ -0,0 +1,6 @@
+from pycode.salary.select_money import select_money
+from pycode.salary.send_money import send_money
+
+if __name__ == '__main__':
+    send_money()
+    select_money()
--- start.py
+++
@@ -1,6 +0,0 @@
-from pycode.select_money import select_money
-from pycode.send_money import send_money
-
-if __name__ == '__main__':
-    send_money()
-    select_money()
